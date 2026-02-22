403aabe28e92ba0b4616e42265ca6a1444ea703f
daimmartinez <daim.martinez@uabc.edu.mx>
2026-02-21 19:18:16 -0800

    Initial commit

--- js/script.js
+++ js/script.js
@@ -165,3 +165,27 @@
   });
 
 })(jQuery);
+
+
+
+const btn = document.getElementById('button');
+
+document.getElementById('form')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   btn.value = 'Sending...';
+
+   const serviceID = 'default_service';
+   const templateID = 'template_ngzeff9';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Send Email';
+      alert('Correo electrónico enviado!');
+    }, (err) => {
+      btn.value = 'Send Email';
+      alert(JSON.stringify(err));
+    });
+});
+
